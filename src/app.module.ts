abf417430a642da1987f561d833e926130456a27
Paula-Refaat <poularefaat86@gmail.com>
2024-11-14 06:57:12 +0200

    💸 middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
-import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { UserModule } from './users/users.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { WrapDataInterceptor } from './common/interceptors/wrap-interceptor/wrap-data.interceptor';
 import { CommonModule } from './common/common.module';
+import { LoggerMiddleware } from './common/middlewares/logger/logger.middleware';
+import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [UserModule, CommonModule],
@@ -11,4 +19,11 @@ import { CommonModule } from './common/common.module';
     // { provide: APP_INTERCEPTOR, useClass: WrapDataInterceptor },
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude({ path: 'user/:id', method: RequestMethod.PATCH }) // exclude patch method for user
+      .forRoutes(UsersController);
+  }
+}
